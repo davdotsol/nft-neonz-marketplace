26f93792781cfe394b6395bb2efe0062734b5f19
davdotsol <davdotsol+github@gmail.com>
2023-12-05 12:39:56 +0100

    Added contract testing

--- test/KryptoNeonz.test.js
+++ test/KryptoNeonz.test.js
@@ -3,8 +3,12 @@ const Neonz = artifacts.require('./KryptoNeonz');
 
 require('chai').use(require('chai-as-promised')).should();
 
-contract('KryptoNeonz', async (accounts) => {
-  let contract = await Neonz.deployed();
+contract('KryptoNeonz', (accounts) => {
+  let contract;
+
+  before(async () => {
+    contract = await Neonz.deployed();
+  });
 
   describe('deployment', async () => {
     it('deploys successfully', async () => {
@@ -32,8 +36,35 @@ contract('KryptoNeonz', async (accounts) => {
 
       assert.equal(totalSupply, 1);
       const event = result.logs[0].args;
-      assert.equal(event.from, '0x0', 'from is the contract');
-      assert.equal(event.to, '');
+      assert.equal(
+        event._from,
+        '0x0000000000000000000000000000000000000000',
+        'from is the contract'
+      );
+      assert.equal(event._to, accounts[0], 'to is msg.sender');
+
+      // Failure
+      await contract.mint('https://1.png').should.be.rejected;
+    });
+  });
+
+  describe('indexing', async () => {
+    it('lists KryptoNeonz', async () => {
+      await contract.mint('https://2.png');
+      await contract.mint('https://3.png');
+      await contract.mint('https://4.png');
+      const totalSupply = await contract.totalSupply();
+      const result = [];
+      for (i = 1; i <= totalSupply; i++) {
+        result.push(await contract.neonz(i - 1));
+      }
+      const expected = [
+        'https://1.png',
+        'https://2.png',
+        'https://3.png',
+        'https://4.png',
+      ];
+      assert.equal(result.join(','), expected.join(','));
     });
   });
 });
